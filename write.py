4c1d19692928289f7142c6d234ecb5baba631c50
kracken9500 <evanm831@gmail.com>
2019-04-26 18:08:27 -0500

    finished up seeding and word selection

--- write.py
+++ write.py
@@ -1,8 +1,17 @@
 import random
+import hashlib
+from PIL import Image
+import io
 
 
-def set_seed(img):
-    random.seed(100)
+def set_seed(loc):
+    img = Image.open(loc)
+    m = hashlib.sha256()
+    with io.BytesIO() as mem:
+        img.save(mem, 'PNG')
+        data = mem.getvalue()
+        m.update(data)
+    random.seed(m.digest())
 
 
 class WordBank:
@@ -53,11 +62,60 @@ class WordBank:
             self.l7.append(x)
 
     def pull_word(self, syl):
-        pass
+        if syl == 1:
+            return self.l1[random.randint(0, len(self.l1) - 1)]
+        elif syl == 2:
+            return self.l2[random.randint(0, len(self.l2) - 1)]
+        elif syl == 3:
+            return self.l3[random.randint(0, len(self.l3) - 1)]
+        elif syl == 4:
+            return self.l4[random.randint(0, len(self.l4) - 1)]
+        elif syl == 5:
+            return self.l5[random.randint(0, len(self.l5) - 1)]
+        elif syl == 6:
+            return self.l6[random.randint(0, len(self.l6) - 1)]
+        elif syl == 7:
+            return self.l7[random.randint(0, len(self.l7) - 1)]
+        else:
+            return ""
+
+
+def chop(total):
+    weights = [10, 50, 40, 20, 15, 12, 10]
+    upper = sum(weights) - 1
+    bounds = []
+    ret = []
+    for x in weights:
+        bounds.append(sum(bounds) + x)
+    while total > 0:
+        r = random.randint(0, upper)
+        bins = 1
+        for x in range(1, len(bounds)):
+            bins = x
+            if r < bounds[x]:
+                break
+        if bins <= total:
+            ret.append(bins)
+            total -= bins
+    return ret
+
+
+def gen_line(wb, size):
+    bins = chop(size)
+    line = ''
+    for x in bins:
+        line = line + wb.pull_word(x).rstrip() + ' '
+    return line[:len(line) - 1]
+
+
+def gen_haiku(wb):
+    return gen_line(wb, 5) + '\n' + gen_line(wb, 7) + '\n' + gen_line(wb, 5)
 
 
 def main():
-    pass
+    set_seed('test.jpg')
+    wb = WordBank()
+    print(gen_haiku(wb))
 
 
 if __name__ == "__main__":
